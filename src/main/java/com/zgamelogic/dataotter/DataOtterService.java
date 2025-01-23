d57ae6ba569292ac1dadda106b942ab49345acb7
Ben Shabowski <benshabowski@gmail.com>
2025-01-23 14:18:46 -0600

    removing import

--- src/main/java/com/zgamelogic/dataotter/DataOtterService.java
+++ src/main/java/com/zgamelogic/dataotter/DataOtterService.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zgamelogic.dataotter.data.Monitor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
